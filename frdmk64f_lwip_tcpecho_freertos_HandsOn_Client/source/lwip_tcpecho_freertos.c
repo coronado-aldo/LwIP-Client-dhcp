300ac08ec6d3af5d5581b353cc5c365c9556d7ce
Aldo <aldo.coronado@venteks.com>
2020-09-22 12:28:02 -0500

    Cambios DHCP

--- frdmk64f_lwip_tcpecho_freertos_HandsOn_Client/source/lwip_tcpecho_freertos.c
+++ frdmk64f_lwip_tcpecho_freertos_HandsOn_Client/source/lwip_tcpecho_freertos.c
@@ -19,7 +19,7 @@
 #include "lwip/tcpip.h"
 #include "netif/ethernet.h"
 #include "enet_ethernetif.h"
-
+#include "lwip/prot/dhcp.h"
 #include "board.h"
 #include "fsl_phy.h"
 
@@ -31,7 +31,7 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-
+#if 0
 /* IP address configuration. */
 #define configIP_ADDR0 192
 #define configIP_ADDR1 168
@@ -50,12 +50,14 @@
 #define configGW_ADDR2 0
 #define configGW_ADDR3 100
 
+#endif
+
 
 
 /* MAC address configuration. */
 #define configMAC_ADDR                     \
     {                                      \
-        0x02, 0x12, 0x13, 0x10, 0x15, 0x11 \
+        0x02, 0x12, 0x13, 0x10, 0x15, 0x15 \
     }
 
 /* Address of PHY interface. */
@@ -75,6 +77,12 @@
 #define EXAMPLE_NETIF_INIT_FN ethernetif0_init
 #endif /* EXAMPLE_NETIF_INIT_FN */
 
+/*! @brief Stack size of the thread which prints DHCP info. */
+#define PRINT_THREAD_STACKSIZE 512
+
+/*! @brief Priority of the thread which prints DHCP info. */
+#define PRINT_THREAD_PRIO DEFAULT_THREAD_PRIO
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -89,6 +97,86 @@ static phy_handle_t phyHandle   = {.phyAddr = EXAMPLE_PHY_ADDRESS, .mdioHandle =
 /*******************************************************************************
  * Code
  ******************************************************************************/
+/*!
+ * @brief Prints DHCP status of the interface when it has changed from last status.
+ *
+ * @param arg pointer to network interface structure
+ */
+static void print_dhcp_state(void *arg)
+{
+    struct netif *netif = (struct netif *)arg;
+    struct dhcp *dhcp;
+    u8_t dhcp_last_state = DHCP_STATE_OFF;
+
+    while (netif_is_up(netif))
+    {
+        dhcp = netif_dhcp_data(netif);
+
+        if (dhcp == NULL)
+        {
+            dhcp_last_state = DHCP_STATE_OFF;
+        }
+        else if (dhcp_last_state != dhcp->state)
+        {
+            dhcp_last_state = dhcp->state;
+
+            PRINTF(" DHCP state       : ");
+            switch (dhcp_last_state)
+            {
+                case DHCP_STATE_OFF:
+                    PRINTF("OFF");
+                    break;
+                case DHCP_STATE_REQUESTING:
+                    PRINTF("REQUESTING");
+                    break;
+                case DHCP_STATE_INIT:
+                    PRINTF("INIT");
+                    break;
+                case DHCP_STATE_REBOOTING:
+                    PRINTF("REBOOTING");
+                    break;
+                case DHCP_STATE_REBINDING:
+                    PRINTF("REBINDING");
+                    break;
+                case DHCP_STATE_RENEWING:
+                    PRINTF("RENEWING");
+                    break;
+                case DHCP_STATE_SELECTING:
+                    PRINTF("SELECTING");
+                    break;
+                case DHCP_STATE_INFORMING:
+                    PRINTF("INFORMING");
+                    break;
+                case DHCP_STATE_CHECKING:
+                    PRINTF("CHECKING");
+                    break;
+                case DHCP_STATE_BOUND:
+                    PRINTF("BOUND");
+                    break;
+                case DHCP_STATE_BACKING_OFF:
+                    PRINTF("BACKING_OFF");
+                    break;
+                default:
+                    PRINTF("%u", dhcp_last_state);
+                    assert(0);
+                    break;
+            }
+            PRINTF("\r\n");
+
+            if (dhcp_last_state == DHCP_STATE_BOUND)
+            {
+                PRINTF("\r\n IPv4 Address     : %s\r\n", ipaddr_ntoa(&netif->ip_addr));
+                PRINTF(" IPv4 Subnet mask : %s\r\n", ipaddr_ntoa(&netif->netmask));
+                PRINTF(" IPv4 Gateway     : %s\r\n\r\n", ipaddr_ntoa(&netif->gw));
+            }
+        }
+
+        sys_msleep(20U);
+    }
+
+    vTaskDelete(NULL);
+}
+
 
 /*!
  * @brief Main function
@@ -117,9 +205,9 @@ int main(void)
 
     mdioHandle.resource.csrClock_Hz = EXAMPLE_CLOCK_FREQ;
 
-    IP4_ADDR(&netif_ipaddr, configIP_ADDR0, configIP_ADDR1, configIP_ADDR2, configIP_ADDR3);
-    IP4_ADDR(&netif_netmask, configNET_MASK0, configNET_MASK1, configNET_MASK2, configNET_MASK3);
-    IP4_ADDR(&netif_gw, configGW_ADDR0, configGW_ADDR1, configGW_ADDR2, configGW_ADDR3);
+    IP4_ADDR(&netif_ipaddr, 0U, 0U, 0U, 0U);
+	IP4_ADDR(&netif_netmask, 0U, 0U, 0U, 0U);
+	IP4_ADDR(&netif_gw, 0U, 0U, 0U, 0U);
 
     tcpip_init(NULL, NULL);
 
@@ -128,16 +216,16 @@ int main(void)
     netifapi_netif_set_default(&netif);
     netifapi_netif_set_up(&netif);
 
+    netifapi_dhcp_start(&netif);
+
     PRINTF("\r\n************************************************\r\n");
-    PRINTF(" TCP Echo example\r\n");
+    PRINTF(" TCP Echo Client with DHCP\r\n");
     PRINTF("************************************************\r\n");
-    PRINTF(" IPv4 Address     : %u.%u.%u.%u\r\n", ((u8_t *)&netif_ipaddr)[0], ((u8_t *)&netif_ipaddr)[1],
-           ((u8_t *)&netif_ipaddr)[2], ((u8_t *)&netif_ipaddr)[3]);
-    PRINTF(" IPv4 Subnet mask : %u.%u.%u.%u\r\n", ((u8_t *)&netif_netmask)[0], ((u8_t *)&netif_netmask)[1],
-           ((u8_t *)&netif_netmask)[2], ((u8_t *)&netif_netmask)[3]);
-    PRINTF(" IPv4 Gateway     : %u.%u.%u.%u\r\n", ((u8_t *)&netif_gw)[0], ((u8_t *)&netif_gw)[1],
-           ((u8_t *)&netif_gw)[2], ((u8_t *)&netif_gw)[3]);
-    PRINTF("************************************************\r\n");
+
+    if (sys_thread_new("print_dhcp", print_dhcp_state, &netif, PRINT_THREAD_STACKSIZE, PRINT_THREAD_PRIO) == NULL)
+	{
+		LWIP_ASSERT("stack_init(): Task creation failed.", 0);
+	}
 
     tcpecho_init();
 
